615a345a956d2427445ef8dda62c6c1cb8ada80d
Ildar <example@gmail.com>
2021-06-07 09:42:24 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = '5';
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+// console.log(obj.name);
+// console.log(obj['love']);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1])
+
+// _____________________________________________________________________
+
+
+// alert('Hello');
+
+const result = confirm('are you here?');
